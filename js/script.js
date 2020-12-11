bf7fdfb8f6a58bb55276676b5ca0d5694ed13d18
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-11 15:26:28 +0100

    work complete

--- js/script.js
+++ js/script.js
@@ -6,10 +6,16 @@ alert("Rispondi alle prossime semplici domande e avrai la password perfetta per 
 // domande
 
 var nome = prompt("Come ti chiami?");
+var iniziale_nome = nome.charAt(0);
+var iniziale_nome_grande = iniziale_nome.toUpperCase();
+var restante_nome = nome.substring(1);
 
 console.log(nome);
 
 var cognome = prompt("Perfetto, ora anche il tuo cognome grazie!");
+var iniziale_cognome = cognome.charAt(0);
+var iniziale_cognome_grande = iniziale_cognome.toUpperCase();
+var restante_cognome = cognome.substring(1);
 
 console.log(cognome);
 
@@ -21,7 +27,7 @@ alert("Ottimo, ora generermo la nostr.. ehm la tua password!")
 
 // password
 
-var totale = nome + cognome + colore + "19";
+var totale = iniziale_nome_grande + restante_nome + iniziale_cognome_grande + restante_cognome + colore + "19";
 
 document.getElementById('password').innerHTML= "Ecco qui la tua password: " + totale;
 
